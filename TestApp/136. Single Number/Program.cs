fe38ebfa36613cd3072f7d45d4b762df308efc89
yacoquss <yacoquss@mail.ru>
2025-03-03 13:37:40 +0300

    create

---
+++ TestApp/136. Single Number/Program.cs
@@ -0,0 +1,12 @@
+﻿var sol = new Solution();
+int[] nums = [4, 1, 2, 1, 2];
+Console.WriteLine(sol.SingleNumber(nums));
+
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        return 1;
+    }
+}
