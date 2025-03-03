0c69a53b8fb3629fbd9733bef2340cbe323c8986
yacoquss <yacoquss@mail.ru>
2025-03-03 13:57:49 +0300

    solution

--- Leetcode/136. Single Number/Program.cs
+++ Leetcode/136. Single Number/Program.cs
@@ -1,5 +1,5 @@
 ﻿var sol = new Solution();
-int[] nums = [4, 1, 2, 1, 2];
+int[] nums = [4,1,2,1,2];
 Console.WriteLine(sol.SingleNumber(nums));
 
 
@@ -7,6 +7,16 @@ public class Solution
 {
     public int SingleNumber(int[] nums)
     {
-        return 1;
+        var elementCounts = nums
+            .GroupBy(x => x)
+            .Select(g => new { Element = g.Key, Count = g.Count() });
+
+
+        foreach (var item in elementCounts
+                     .Where(x => x.Count == 1))
+        {
+            return item.Element;
+        }
+        return nums[0];
     }
 }
