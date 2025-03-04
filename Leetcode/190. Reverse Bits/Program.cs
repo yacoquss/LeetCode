96c9f5f0f7232c6c105701bdda9a0c52a6024c5b
yacoquss <yacoquss@mail.ru>
2025-03-04 12:47:36 +0300

    solution

---
+++ Leetcode/190. Reverse Bits/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp._190._Reverse_Bits;
+
+public class Solution {
+    public uint reverseBits(uint n) {
+        uint res = 0;
+        for (int i = 0; i < 32; i++) {
+            res = res << 1;       
+            res += (n & 1);        
+            n = n >> 1;            
+        }
+        return res;
+    }
+}
