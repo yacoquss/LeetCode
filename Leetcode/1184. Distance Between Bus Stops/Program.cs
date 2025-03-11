3d73a7788403fd6d47f3b7c52220c20b0949b2af
yacoquss <yacoquss@mail.ru>
2025-03-11 12:46:41 +0300

    ex

---
+++ Leetcode/1184. Distance Between Bus Stops/Program.cs
@@ -0,0 +1,24 @@
+﻿var sol = new Solution();
+int[] nums = [1, 2, 3, 4];
+
+
+
+public class Solution {
+    public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+    {
+        int dist = 0, ccw = 0;
+        if (start > destination)
+        {
+            (start, destination) = (destination, start);
+        }
+
+        while (start != destination)
+        {
+            dist +=distance[start];
+            start++;
+        }
+
+        ccw += distance.Sum() - dist;
+        return Math.Min(dist, ccw);
+    }
+}
