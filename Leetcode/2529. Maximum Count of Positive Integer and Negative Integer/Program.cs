eb7f0cd450c9de98dda97d98a1e8a35f5c27e026
yacoquss <yacoquss@mail.ru>
2025-03-12 12:33:33 +0300

    ex

---
+++ Leetcode/2529. Maximum Count of Positive Integer and Negative Integer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestApp._2529._Maximum_Count_of_Positive_Integer_and_Negative_Integer;
+
+var sol = new Solution();
+int[] nums = [-3, -2, -1, 0, 0, 1, 2];
+Console.WriteLine(sol.MaximumCount(nums));
+
+
+public class Solution {
+    public int MaximumCount(int[] nums) {
+        var pos  = nums.Where(p => p > 0);
+        var neg = nums.Where(p => p < 0);
+        var maxcount = Math.Max(pos.Count(), neg.Count());
+        return maxcount;
+    }
+}
