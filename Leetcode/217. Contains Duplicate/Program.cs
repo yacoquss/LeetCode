0dc546b4c8287f605076bb43a92a7109f8331706
yacoquss <yacoquss@mail.ru>
2025-03-16 13:55:18 +0300

    ex

---
+++ Leetcode/217. Contains Duplicate/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestApp._217._Contains_Duplicate;
+
+
+var sol = new Solution();
+int[] nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+Console.WriteLine(sol.ContainsDuplicate(nums));
+
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var elementsCounts = nums
+            .GroupBy(x => x)
+            .Select(g => new { Element = g.Key, Count = g.Count() });
+
+        foreach (var elementCount in elementsCounts
+                     .Where(x=>x.Count>=2))
+        {
+            return true;
+        }
+        return false;
+    }
+}
