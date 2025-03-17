f121061e5ec604eebb301517bd53ea2921f85090
yacoquss <yacoquss@mail.ru>
2025-03-17 12:19:09 +0300

    ex

---
+++ Leetcode/2206. Divide Array Into Equal Pairs/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestApp._2206._Divide_Array_Into_Equal_Pairs;
+
+
+    
+
+    public class Solution
+    {
+        public bool DivideArray(int[] nums)
+        {
+            var pairsCount = nums.Length / 2;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i+=2)
+            {
+                if (nums[i] != nums[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
