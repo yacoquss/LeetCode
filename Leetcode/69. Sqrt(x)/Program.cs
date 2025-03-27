da0f24ee1bfe506638c2047f169e79b25d9284b7
yacoquss <yacoquss@mail.ru>
2025-03-27 18:25:12 +0300

    ex

---
+++ Leetcode/69. Sqrt(x)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TestApp._69._Sqrt_x_;
+
+public class Program
+{
+}
+
+public class Solution
+{
+    public int MySqrt(int x) {
+        if (x == 0) return 0;
+        
+        double epsilon = 1e-6;
+        double result = x; 
+        double prev;
+        
+        do {
+            prev = result;
+            result = (result + x / result) / 2;
+        } while (Math.Abs(prev - result) > epsilon);
+        
+        return (int)result;
+    }
+}
