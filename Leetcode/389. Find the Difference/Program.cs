5fb9e8cab16b5b5314295faa2435571514c6d966
yacoquss <yacoquss@mail.ru>
2025-03-29 10:55:24 +0300

    ex

---
+++ Leetcode/389. Find the Difference/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TestApp._389._Find_the_Difference;
+
+public class Program
+{
+    
+}
+
+public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        int result = 0;
+
+        foreach (char c in s)
+        {
+            result ^= c;
+        }
+
+        foreach (char c in t)
+        {
+            result ^= c;
+        }
+
+        return (char)result;
+    }
+}
