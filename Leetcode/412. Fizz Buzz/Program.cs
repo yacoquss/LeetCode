1b309e45615f59c3b5dae34e2724ed7826ccaabd
yacoquss <yacoquss@mail.ru>
2025-03-30 13:44:08 +0300

    ex

---
+++ Leetcode/412. Fizz Buzz/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TestApp._412._Fizz_Buzz;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public IList<string> FizzBuzz(int n)
+    {
+        IList<string> result = new List<string>();
+        while (n > 0)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                result.Add("FizzBuzz");
+            }
+            else if (n % 3 == 0)
+            {
+                result.Add("Fizz");
+            }
+            else if (n % 5 == 0)
+            {
+                result.Add("Buzz");
+            }
+            else
+            {
+                result.Add(n.ToString());
+            }
+            n--;
+        }
+        var reversed = result.Reverse().ToList();
+        return reversed;
+    }
+}
