a97698a942d1ca44be88137fcaf23beb819f75e5
yacoquss <yacoquss@mail.ru>
2025-04-07 11:13:17 +0300

    ex

---
+++ Leetcode/1313. Decompress Run-Length Encoded List/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TestApp._1313._Decompress_Run_Length_Encoded_List;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public int[] DecompressRLElist(int[] nums)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            int[] generated = GenerateNums(nums[i], nums[i + 1]);
+            result.AddRange(generated);
+        }
+        return result.ToArray();
+    }
+
+    private int[] GenerateNums(int freq, int val)
+    {
+        int[] nums = new int[freq];
+        for (int i = 0; i < freq; i++)
+        {
+            nums[i] = val;
+        }
+        return nums;
+    }
+}
