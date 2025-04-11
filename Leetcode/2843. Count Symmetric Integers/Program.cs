f55708126b2635ad656f2af8c28baaa21a88d8cd
yacoquss <yacoquss@mail.ru>
2025-04-11 20:16:24 +0300

    ex

---
+++ Leetcode/2843. Count Symmetric Integers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TestApp._2843._Count_Symmetric_Integers;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public int CountSymmetricIntegers(int low, int high) => 
+        Enumerable.Range(low, (high - low) + 1).
+            Select(i => i.ToString().ToCharArray()).
+            Where(m => m.Length % 2 == 0 &&
+                       m.Take(m.Length / 2).Sum(m => char.GetNumericValue(m)) ==
+                       m.Skip(m.Length / 2).Sum(m => char.GetNumericValue(m))).
+            Count();
+}
