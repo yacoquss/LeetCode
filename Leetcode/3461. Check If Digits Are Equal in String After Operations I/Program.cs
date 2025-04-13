afd70e9e4ce3f89ce7d916120db2226a840b48fc
yacoquss <yacoquss@mail.ru>
2025-04-13 20:01:34 +0300

    ex

---
+++ Leetcode/3461. Check If Digits Are Equal in String After Operations I/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestApp._3461._Check_If_Digits_Are_Equal_in_String_After_Operations_I;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public bool HasSameDigits(string s) {
+        while(s.Length > 2)
+        {     
+            var val = ""; 
+            for(int i=0; i<s.Length-1; i++)
+            {
+                val = val + ((s[i] + s[i+1]) % 10);
+            }
+            s = val;
+        }
+        return s[0].Equals(s[1]) ? true : false;
+    }
+}
