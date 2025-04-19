48dc92fcd17b45b638372984a7212b3f2f526fa8
yacoquss <yacoquss@mail.ru>
2025-04-19 19:51:41 +0300

    ex

---
+++ Leetcode/1002. Find Common Characters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestApp._1002._Find_Common_Characters;
+
+public class Program
+{
+    
+}
+
+public class Solution
+{
+    public IList<string> CommonChars(string[] words)
+    {
+        var result = new List<string>();
+
+        for (var i = 'a'; i <= 'z'; i++)
+        {
+            var count = words.Min(w => w.Count(c => c == i));
+
+            while (--count >= 0)
+                result.Add(i.ToString());
+        }
+
+        return result;
+    }
+}
