968f88c8c808125923178542cd7ff18912bf0e79
yacoquss <yacoquss@mail.ru>
2025-04-21 21:29:49 +0300

    ex

---
+++ Leetcode/3498. Reverse Degree of a String/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TestApp._3498._Reverse_Degree_of_a_String;
+
+
+public class Program
+{
+    
+}
+
+public class Solution
+{
+    public int ReverseDegree(string s)
+    {
+        int sum = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            
+            if (c >= 'a' && c <= 'z')
+            {
+                
+                int reverseAlphabetPosition = 26 - (c - 'a');
+                
+                int stringPosition = i + 1;
+                sum += reverseAlphabetPosition * stringPosition;
+            }
+        }
+        return sum;
+    }
+}
