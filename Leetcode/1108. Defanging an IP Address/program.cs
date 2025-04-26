6c4e2d230ce73061ec9be4ef24f2e5789475dd3d
yacoquss <yacoquss@mail.ru>
2025-04-26 15:22:34 +0300

    ex

---
+++ Leetcode/1108. Defanging an IP Address/program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp._1108._Defanging_an_IP_Address;
+
+public class program
+{
+    
+}
+
+
+public class Solution {
+    public string DefangIPaddr(string address) {
+        return address.Replace(".", "[.]");
+    }
+}
