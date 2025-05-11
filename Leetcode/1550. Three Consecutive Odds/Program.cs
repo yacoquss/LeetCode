9ab318ac9df517627b4d68003d67ab3a8919a29d
yacoquss <yacoquss@mail.ru>
2025-05-11 14:16:14 +0300

    ex

---
+++ Leetcode/1550. Three Consecutive Odds/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TestApp._1550._Three_Consecutive_Odds;
+
+public class Program
+{
+    
+}
+public class Solution
+{
+    public bool ThreeConsecutiveOdds(int[] arr)
+    {
+        for (int i = 0; i <= arr.Length - 3; i++)
+        {
+            if (IsOdd(arr[i]) & IsOdd(arr[i + 1]) & IsOdd(arr[i + 2]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsOdd(int num)
+    {
+        return num % 2 != 0;
+    }
+}
