2a3c561037c8eeb7888737ffff587de3d70958fe
yacoquss <yacoquss@mail.ru>
2025-05-24 21:32:29 +0300

    ex

---
+++ Leetcode/2942. Find Words Containing Character/program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestApp._2942._Find_Words_Containing_Character;
+
+public class program
+{
+    
+}
+public class Solution
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        IList<int> result = new List<int>();
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
+}
