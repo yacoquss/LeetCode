2f2393c618518ca7ef422edf5f4641eb9284ee12
yacoquss <yacoquss@mail.ru>
2025-05-25 13:27:44 +0300

    ex

---
+++ Leetcode/657. Robot Return to Origin/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestApp._657._Robot_Return_to_Origin;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public bool JudgeCircle(string moves)
+    {
+        int Xpos = 0, Ypos = 0;
+        foreach (var c in moves)
+        {
+            if (c == 'L') Xpos--;
+            if (c == 'R') Xpos++;
+            if (c == 'U') Ypos--;
+            if (c == 'D') Ypos++;
+        }
+
+        if (Xpos == 0 && Ypos == 0) return true;
+        else return false;
+        
+    }
+}
