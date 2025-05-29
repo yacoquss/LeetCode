eabe007c81d33266ecac28471bd2d8f2565d8191
yacoquss <yacoquss@mail.ru>
2025-05-29 16:27:19 +0300

    ex

---
+++ Leetcode/942. DI String Match/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestApp._942._DI_String_Match;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public int[] DiStringMatch(string s) {
+        int i = 0;
+        int j = s.Length;
+        int[] res = new int[j + 1];
+        for(int k = 0; k < s.Length; k++){
+            if(s[k] == 'I') res[k] = i++;
+            else res[k] = j--;
+        }
+        res[res.Length - 1] = i;
+        return res;
+    }
+}
