392dee37f2347066dca4cbf2b17845b4eef57ff6
yacoquss <yacoquss@mail.ru>
2025-05-31 14:39:17 +0300

    ex

---
+++ Leetcode/771. Jewels and Stones/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApp._771._Jewels_and_Stones;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        int count = 0;
+        for (var i = 0; i < stones.Length; i++)
+        {
+            if (jewels.Contains(stones[i]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
