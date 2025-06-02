f87f5b650c2ecdefc08ec1d574a3dc736efb8eab
yacoquss <yacoquss@mail.ru>
2025-06-02 21:26:15 +0300

    ex

---
+++ Leetcode/709. To Lower Case/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp._709._To_Lower_Case;
+
+public class Program
+{
+    
+}
+
+public class Solution {
+    public string ToLowerCase(string s)
+    {
+        return s.ToLower();
+    }
+}
