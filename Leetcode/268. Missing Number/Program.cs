8c531c530618023c2f9dda00d84d48257ae2de6b
yacoquss <yacoquss@mail.ru>
2025-06-07 22:23:24 +0300

    ex

---
+++ Leetcode/268. Missing Number/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestApp._268._Missing_Number;
+
+public class Program
+{
+    
+}
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int max = nums.Max();
+        for (int i = 0; i <= max+1; i++)
+        {
+            if (!nums.Contains(i)) return i;
+        }
+
+        return -1;
+    }
+}
