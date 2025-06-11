91670b6936b5c55b942ae81ab64fe220128a6c34
yacoquss <yacoquss@mail.ru>
2025-06-11 14:28:09 +0300

    ex

---
+++ Leetcode/884. Uncommon Words from Two Sentences/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TestApp._884._Uncommon_Words_from_Two_Sentences;
+
+public class Program
+{
+    public string[] UncommonFromSentences(string s1, string s2) 
+    {
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+    
+        // Подсчитываем все слова из первого предложения
+        foreach (string word in s1.Split())
+        {
+            if (wordCount.ContainsKey(word))
+                wordCount[word]++;
+            else
+                wordCount[word] = 1;
+        }
+    
+        // Подсчитываем все слова из второго предложения
+        foreach (string word in s2.Split())
+        {
+            if (wordCount.ContainsKey(word))
+                wordCount[word]++;
+            else
+                wordCount[word] = 1;
+        }
+    
+        // Выбираем слова, которые встречаются ровно один раз
+        return wordCount.Where(pair => pair.Value == 1)
+            .Select(pair => pair.Key)
+            .ToArray();
+    }
+}
