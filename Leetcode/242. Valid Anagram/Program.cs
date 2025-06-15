346b04cf231a20c0c932aa15cde4f6cfc67c01b7
yacoquss <yacoquss@mail.ru>
2025-06-15 15:44:25 +0300

    ex

---
+++ Leetcode/242. Valid Anagram/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestApp._242._Valid_Anagram;
+
+public class Program
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        char[] sChars = s.ToCharArray();
+        char[] tChars = t.ToCharArray();
+
+        Array.Sort(sChars);
+        Array.Sort(tChars);
+
+        return new string(sChars) == new string(tChars);
+    }
+}
