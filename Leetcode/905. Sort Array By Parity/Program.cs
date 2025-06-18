4a116eb80502b28766e3cc32b93b8da9b849f11b
yacoquss <yacoquss@mail.ru>
2025-06-18 18:15:48 +0300

    ex

---
+++ Leetcode/905. Sort Array By Parity/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestApp._905._Sort_Array_By_Parity;
+
+public class Program
+{
+    public int[] SortArrayByParity(int[] nums) {
+        List<int> odds = new List<int>();
+        List<int> evens = new List<int>();
+        foreach (var num in nums)
+        {
+            if(num % 2 == 0) odds.Add(num);
+            else evens.Add(num);
+        }
+        
+        return (odds.Concat(evens)).ToArray();
+        
+    }
+}
