7a18a662aa76975ce6a58a02894582972a236106
yacoquss <yacoquss@mail.ru>
2025-06-22 13:10:05 +0300

    ex

---
+++ Leetcode/2138. Divide a String Into Groups of Size k/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestApp._2138._Divide_a_String_Into_Groups_of_Size_k;
+
+public class Program
+{
+    public string[] DivideString(string s, int k, char fill)
+    {
+        List<string> result = new List<string>();
+        for (int i = 0; i < s.Length; i += k)
+        {
+            int length = Math.Min(k, s.Length - i);
+            string group = s.Substring(i, length);
+            if (group.Length < k)
+            {
+                group += new string(fill, k - group.Length);
+            }
+            result.Add(group);
+        }
+        return result.ToArray();
+    }
+}
