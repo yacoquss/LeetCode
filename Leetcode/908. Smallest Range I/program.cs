52338ca20143fc1053fdb298c7b3ded7ece643ec
yacoquss <yacoquss@mail.ru>
2025-06-26 18:27:06 +0300

    ex

---
+++ Leetcode/908. Smallest Range I/program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestApp._908._Smallest_Range_I;
+
+public class program
+{
+    public int SmallestRangeI(int[] nums, int k) 
+    {
+        int min_original = nums.Min();
+        int max_original = nums.Max();
+    
+        if (max_original - min_original <= 2 * k)
+        {
+            return 0;
+        }
+        else
+        {
+            return (max_original - k) - (min_original + k);
+        }
+    }
+}
