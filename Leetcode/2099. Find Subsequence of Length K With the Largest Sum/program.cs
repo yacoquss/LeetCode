134306396bbb5dd4ff7676d14a7b8dcb05276210
yacoquss <yacoquss@mail.ru>
2025-06-28 14:12:00 +0300

    ex

---
+++ Leetcode/2099. Find Subsequence of Length K With the Largest Sum/program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApp._2099._Find_Subsequence_of_Length_K_With_the_Largest_Sum;
+
+public class program
+{
+    public int[] MaxSubsequence(int[] nums, int k) {
+      
+        var indexedNums = nums.Select((num, index) => (num, index)).ToArray();
+        Array.Sort(indexedNums, (a, b) => b.num.CompareTo(a.num)); 
+    
+        var topK = indexedNums.Take(k).OrderBy(x => x.index).ToArray();
+    
+        return topK.Select(x => x.num).ToArray();
+    }
+}
