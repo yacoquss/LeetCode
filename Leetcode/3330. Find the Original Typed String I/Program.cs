785e93383b7d6d6b53db2bc966d85190d961ab95
yacoquss <yacoquss@mail.ru>
2025-07-01 21:05:28 +0300

    ex

---
+++ Leetcode/3330. Find the Original Typed String I/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApp._3330._Find_the_Original_Typed_String_I;
+
+public class Program
+{
+    
+}
+public class Solution {
+    public int PossibleStringCount(string word) => Enumerable.
+        Range(0, word.Length - 1).
+        Sum(m => word[m] == word[m + 1] ? 1 : 0) + 1;
+}
