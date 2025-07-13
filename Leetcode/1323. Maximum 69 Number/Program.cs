0c8a2ed8c27b4bc657f0dbd252c0250a0e94018a
yacoquss <yacoquss@mail.ru>
2025-07-13 14:13:16 +0300

    ex

---
+++ Leetcode/1323. Maximum 69 Number/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestApp._1323._Maximum_69_Number;
+
+public class Program
+{
+    public int Maximum69Number(int num)
+    {
+        var s = num.ToString().ToCharArray();
+        char six = Convert.ToChar('9');
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '6')
+            {
+                s[i] = six;
+
+                return Convert.ToInt32(new string(s));
+            }
+        }
+
+        return num;
+    }
+}
