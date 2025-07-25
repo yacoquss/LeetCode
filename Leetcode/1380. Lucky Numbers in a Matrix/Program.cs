e14edcc7f76e61d28178621aefbd4b6f95818d31
yacoquss <yacoquss@mail.ru>
2025-07-25 18:29:39 +0300

    ex

---
+++ Leetcode/1380. Lucky Numbers in a Matrix/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TestApp._1380._Lucky_Numbers_in_a_Matrix;
+
+public class Solution {
+    public IList<int> LuckyNumbers(int[][] matrix) {
+        HashSet<int> minRows = new HashSet<int>();
+        foreach (var row in matrix) {
+            minRows.Add(row.Min());
+        }
+
+        HashSet<int> maxCols = new HashSet<int>();
+        for (int col = 0; col < matrix[0].Length; col++) {
+            int max = matrix[0][col];
+            for (int row = 1; row < matrix.Length; row++) {
+                if (matrix[row][col] > max) {
+                    max = matrix[row][col];
+                }
+            }
+            maxCols.Add(max);
+        }
+
+        return minRows.Intersect(maxCols).ToList();
+    }
+}
