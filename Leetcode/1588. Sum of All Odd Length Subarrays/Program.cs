eb9cf94ed3f8b8797f5a9c5aa8587bda2a2c2da6
yacoquss <yacoquss@mail.ru>
2025-07-31 21:16:49 +0300

    ex

---
+++ Leetcode/1588. Sum of All Odd Length Subarrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestApp._1588._Sum_of_All_Odd_Length_Subarrays;
+
+public class Program
+{
+    public class Solution {
+    public int SumOddLengthSubarrays(int[] arr) {
+        int count = arr.Length;
+        int result = 0;
+        for(int i = 0; i < arr.Length; i ++)
+        {
+            int j = (i + 1)*(count - i)/2;
+            if((i + 1)*(count - i)%2 != 0) j+=1;
+            result += arr[i] * j;
+        }
+        
+        return result;
+    }
+}
+}
