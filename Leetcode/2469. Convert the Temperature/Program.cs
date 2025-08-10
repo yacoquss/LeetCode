899eda9c73a216710fe2cf60f59b951d8b278245
yacoquss <yacoquss@mail.ru>
2025-08-10 20:35:24 +0300

    ex

---
+++ Leetcode/2469. Convert the Temperature/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApp._2469._Convert_the_Temperature;
+
+public class Program
+{
+    public double[] ConvertTemperature(double celsius)
+    {
+        return new double[] { celsius + 273.15, celsius * 1.80 + 32.00 };
+    }
+}
