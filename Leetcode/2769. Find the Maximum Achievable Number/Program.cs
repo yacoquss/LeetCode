ffe4859ae5a20815300c5bfd07503d8ca03e70ad
yacoquss <yacoquss@mail.ru>
2025-08-10 20:31:11 +0300

    ex

---
+++ Leetcode/2769. Find the Maximum Achievable Number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApp._2769._Find_the_Maximum_Achievable_Number;
+
+public class Program
+{
+    public int TheMaximumAchievableX(int num, int t)
+    {
+        
+        return num + 2 * t;
+    }
+}
