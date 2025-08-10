b99c0f606033fd35f272190b34e37e69fbbb0f10
yacoquss <yacoquss@mail.ru>
2025-08-10 20:22:30 +0300

    ex

---
+++ Leetcode/2894. Divisible and Non-divisible Sums Difference/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestApp._2894._Divisible_and_Non_divisible_Sums_Difference;
+
+public class Program
+{
+    public int DifferenceOfSums(int n, int m)
+    {
+        int sumDivisible = 0;
+        int sumNotDivisible = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % m == 0)
+            {
+                sumDivisible += i;
+            }
+            else
+            {
+                sumNotDivisible += i;
+            }
+        }
+
+        return sumNotDivisible - sumDivisible;
+    }
+}
