50895eb680c340a424c328bfa4ba08106a1c3b65
yacoquss <yacoquss@mail.ru>
2025-08-10 20:13:14 +0300

    ex

---
+++ Leetcode/3110. Score of a String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace TestApp._3110._Score_of_a_String;
+
+public class Program
+{
+    public int ScoreOfString(string s)
+    {
+        byte[] asciiBytes = Encoding.ASCII.GetBytes(s);
+        int sum = 0;
+        for (int i = 0; i < asciiBytes.Length - 1; i++)
+        {
+            sum += Math.Abs(asciiBytes[i] - asciiBytes[i + 1]);
+        }
+
+        return sum;
+    }
+}
