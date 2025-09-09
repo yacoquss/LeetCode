d3acb5bed9a4ce2d5f827ee8c1182baa0e0b3681
yacoquss <yacoquss@mail.ru>
2025-09-09 23:04:17 +0300

    ex

---
+++ Leetcode/2011. Final Value of Variable After Performing Operations/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApp._2011._Final_Value_of_Variable_After_Performing_Operations;
+
+public class Program
+{
+    
+}
+public class Solution {
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int x = 0;
+        
+        foreach (var operation in operations)
+        {
+            if(operation == "X++" || operation == "++X"){
+                ++x;
+            } else{
+                --x;
+            }
+        }
+        return x;
+    }
+}
