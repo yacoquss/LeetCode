78a5bb9d0be9db865cadb874e6dc614d4dd728a2
yacoquss <yacoquss@mail.ru>
2025-09-09 22:53:21 +0300

    ex

---
+++ Leetcode/1512. Number of Good Pairs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApp._1512._Number_of_Good_Pairs;
+
+public class Program
+{
+    public class Solution {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
