f98c7883f827b940702557b408b4483130279069
yacoquss <yacoquss@mail.ru>
2025-09-09 22:57:33 +0300

    ex

---
+++ Leetcode/3467. Transform Array by Parity/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TestApp._3467._Transform_Array_by_Parity;
+
+public class Program
+{
+    public class Solution {
+        public int[] TransformArray(int[] nums) {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    nums[i] = 0;
+                }
+                else
+                {
+                    nums[i] = 1;
+                }
+            }
+
+            Array.Sort(nums);
+            return nums;
+        }
+    }
+}
