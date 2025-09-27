047651248c636eeb77fecccbfa367272b09308a3
yacoquss <yacoquss@mail.ru>
2025-09-27 18:59:21 +0300

    ex

---
+++ Leetcode/1470. Shuffle the Array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestApp._1470._Shuffle_the_Array;
+
+public class Program
+{
+    public int[] Shuffle(int[] nums, int n) {
+        List<int> result = new List<int>();
+    
+        for (int i = 0; i < n; i++) {
+            result.Add(nums[i]);        
+            result.Add(nums[i + n]);    
+        }
+    
+        return result.ToArray();
+    }
+}
---
+++ Leetcode/1672. Richest Customer Wealth/program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApp._1672._Richest_Customer_Wealth;
+
+public class program
+{
+    public int MaximumWealth(int[][] accounts) {
+        int result = 0;
+
+        foreach (var acc in accounts)
+        {
+            result  = Math.Max(result, acc.Sum());
+        }
+        return result;
+    }
+}
