134e64c4112889c61b3cc6db047ac4faeccb07fb
yacoquss <yacoquss@mail.ru>
2025-09-27 18:20:50 +0300

    ex

---
+++ Leetcode/3516. Find Closest Person/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApp._3516._Find_Closest_Person;
+
+public class Program
+{
+    public int FindClosest(int x, int y, int z) {
+        int dx = Math.Abs(z - x), dy = Math.Abs(z - y);
+        return dx == dy ? 0 : dx < dy ? 1 : 2;
+    }
+}
