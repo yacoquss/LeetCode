82e5d44d141caf6c9cb9705458dbe9f044bfec1b
yacoquss <yacoquss@mail.ru>
2025-09-27 18:34:09 +0300

    ex

---
+++ Leetcode/3190. Find Minimum Operations to Make All Elements Divisible by Three/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestApp._3190._Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three;
+
+public class Program
+{
+    public class Solution
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            return nums.Count(x => x % 3 != 0);
+        }
+    } 
+}
