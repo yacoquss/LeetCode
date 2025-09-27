f9947eed3155b12a30cd8f17d4373da2311236f6
yacoquss <yacoquss@mail.ru>
2025-09-27 18:09:35 +0300

    ex

---
+++ Leetcode/3668. Restore Finishing Order/program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestApp._3668._Restore_Finishing_Order;
+
+public class program
+{
+    public class Solution {
+        public int[] RecoverOrder(int[] order, int[] friends) {
+            List<int> list = new List<int>();
+            foreach (var num in order)
+            {
+                if(friends.Contains(num)) list.Add(num);
+            }
+            return list.ToArray();
+        }
+    }
+}
