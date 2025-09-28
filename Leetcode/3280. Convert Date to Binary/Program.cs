a8ce4fbacb3d51a593cf2af75c1a46bed0af7acf
yacoquss <yacoquss@mail.ru>
2025-09-28 11:50:04 +0300

    ex

---
+++ Leetcode/3280. Convert Date to Binary/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApp._3280._Convert_Date_to_Binary;
+
+public class Program
+{
+    public class Solution {
+        public string ConvertDateToBinary(string date) {
+            _ = DateTime.TryParseExact(date, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out var dt);
+            return Convert.ToString(dt.Year, 2) + "-" + Convert.ToString(dt.Month, 2) + "-" + Convert.ToString(dt.Day, 2);
+        }
+    }
+}
