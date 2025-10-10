f7b0c5d77d48b390e3d3cfd8c90a9daeeb2bd116
yacoquss <yacoquss@mail.ru>
2025-10-10 20:43:31 +0300

    ex

---
+++ Leetcode/3701. Compute Alternating Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestApp._3701._Compute_Alternating_Sum;
+
+public class Program
+{
+    public int AlternatingSum(int[] nums)
+    {
+        int evenSum = 0;
+        int oddSum = 0;
+    
+        // Сумма четных индексов
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            evenSum += nums[i];
+        }
+    
+        // Сумма нечетных индексов
+        for (int i = 1; i < nums.Length; i += 2)
+        {
+            oddSum += nums[i];
+        }
+    
+        return evenSum - oddSum;
+    }
+}
