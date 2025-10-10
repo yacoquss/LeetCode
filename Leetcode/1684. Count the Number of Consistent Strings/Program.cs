db333ea6faa6c469a02649a91ecf662c0589ed9a
yacoquss <yacoquss@mail.ru>
2025-10-10 20:33:38 +0300

    ex

---
+++ Leetcode/1684. Count the Number of Consistent Strings/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApp._1684._Count_the_Number_of_Consistent_Strings;
+
+public class Program
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+        return words.Count(word => word.All(c => allowed.Contains(c)));
+    }
+}
