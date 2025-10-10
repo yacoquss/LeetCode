f0870a8292f7bdc173a26ec1a079a26cda63dcb9
yacoquss <yacoquss@mail.ru>
2025-10-10 22:06:22 +0300

    ex

---
+++ Leetcode/Code Testcase Test Result Test Result 1431. Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApp.Code_Testcase_Test_Result_Test_Result_1431._Kids_With_the_Greatest_Number_of_Candies;
+
+public class Program
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        int greatest = candies.Max();
+        return candies.Select(candy => candy + extraCandies >= greatest).ToList();
+    }
+
+}
