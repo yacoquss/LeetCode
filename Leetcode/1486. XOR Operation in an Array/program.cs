41e77bd4e53d88967db5356bb09e3c44bfe5e849
yacoquss <yacoquss@mail.ru>
2025-10-10 22:16:58 +0300

    ex

---
+++ Leetcode/1486. XOR Operation in an Array/program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApp._1486._XOR_Operation_in_an_Array;
+
+public class program
+{
+    public int XorOperation(int n, int start)
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            list.Add(i * 2 + start);
+        }
+        return list.Aggregate(0, (acc, num) => acc ^ num);
+    }
+}
