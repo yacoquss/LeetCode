6b99f720ab7f0e8e338b32040f0487d8ba05fe76
yacoquss <yacoquss@mail.ru>
2025-10-10 20:55:09 +0300

    ex

---
+++ Leetcode/3512. Minimum Operations to Make Array Sum Divisible by K/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TestApp._3512._Minimum_Operations_to_Make_Array_Sum_Divisible_by_K;
+
+public class Program
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        
+        long totalSum = 0;
+        foreach (var num in nums) totalSum += num;
+    
+        if (totalSum % k == 0) return 0;
+    
+        int remainder = (int)(totalSum % k);
+        int result = remainder; 
+    
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int count = 0;
+            long testSum = totalSum;
+        
+            
+            while (nums[i] - count > 0 && testSum % k != 0)
+            {
+                count++;
+                testSum--;
+            
+                if (testSum % k == 0)
+                {
+                    result = Math.Min(result, count);
+                    break;
+                }
+            }
+        }
+    
+        return result;
+    }
+}
