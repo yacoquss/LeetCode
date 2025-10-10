14f51ff8546ad6d459f56fe2f69d3ab5627e10a6
yacoquss <yacoquss@mail.ru>
2025-10-10 21:05:17 +0300

    ex

---
+++ Leetcode/2413. Smallest Even Multiple/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApp._2413._Smallest_Even_Multiple;
+
+public class Program
+{
+    public int SmallestEvenMultiple(int n)
+    {
+        if (n % 2 == 0) return n;
+        else return (n * 2);
+    }
+}
