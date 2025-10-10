ba91503620b512ab4188021ce4a10cbd35d11ac4
yacoquss <yacoquss@mail.ru>
2025-10-10 20:24:50 +0300

    ex

---
+++ Leetcode/3289. The Two Sneaky Numbers of Digitville/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp._3289._The_Two_Sneaky_Numbers_of_Digitville;
+
+public class Program
+{
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        List<int> result = new List<int>();
+
+        var counts = nums.GroupBy(n => n)
+            .ToDictionary(g => g.Key, g => g.Count());
+        foreach (var num in counts)
+        {
+            if(num.Value == 2) result.Add(num.Key);
+        }
+
+        return result.ToArray();
+    }
+}
