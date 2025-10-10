324d4d060084a7828a73ad3d99eea23688ed6154
yacoquss <yacoquss@mail.ru>
2025-10-10 22:10:13 +0300

    ex

---
+++ Leetcode/1678. Goal Parser Interpretation/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApp._1678._Goal_Parser_Interpretation;
+
+public class Program
+{
+    public string Interpret(string command)
+    {
+        return command.Replace("()", "o").Replace("(al)", "al");
+    }
+}
