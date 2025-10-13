23905fae9cae9c21195437fb6f8094732624d66a
yacoquss <yacoquss@mail.ru>
2025-10-13 19:48:48 +0300

    ex

---
+++ Leetcode/2798. Number of Employees Who Met the Target/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApp._2798._Number_of_Employees_Who_Met_the_Target;
+
+public class Program
+{
+    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    {
+        int count = 0;
+        foreach (var hour in hours)
+        {
+            if(hour>=target)  count++;
+        }
+        return count;
+    }
+}
