c47385cef89d41fe48f6d3be90cc700322af23ac
yacoquss <yacoquss@mail.ru>
2025-10-13 20:19:09 +0300

    ex

---
+++ Leetcode/2520. Count the Digits That Divide a Number/program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApp._2520._Count_the_Digits_That_Divide_a_Number;
+
+public class program
+{
+    public class Solution {
+        public int CountDigits(int num) {
+            int count = 0;
+            int temp = num;
+        
+            
+            while (temp > 0) {
+                int digit = temp % 10;  
+                if (digit != 0 && num % digit == 0) {
+                    count++;
+                }
+                temp /= 10;  
+            }
+        
+            return count;
+        }
+    }
+}
