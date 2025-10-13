33b67ee8938ab814852c0d168979283bc8d37017
yacoquss <yacoquss@mail.ru>
2025-10-13 20:04:18 +0300

    ex

---
+++ Leetcode/2824. Count Pairs Whose Sum is Less than Target/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TestApp._2824._Count_Pairs_Whose_Sum_is_Less_than_Target;
+
+public class Program
+{
+    public class Solution
+    {
+        public int CountPairs(IList<int> nums, int target)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = i + 1; j < nums.Count; j++)
+                {
+                    if (i < j)
+                    {
+                        if (nums[i] + nums[j] < target)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        
+            return count;
+        }
+    }
+}
