5a807df5fec6e8044878a292fe19ceef0c39ec60
yacoquss <yacoquss@mail.ru>
2025-10-13 20:12:05 +0300

    ex

---
+++ Leetcode/1480. Running Sum of 1d Array/program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp._1480._Running_Sum_of_1d_Array;
+
+public class program
+{
+    public class Solution {
+        public int[] RunningSum(int[] nums) {
+            for (int i = 1; i < nums.Length; i++) {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
