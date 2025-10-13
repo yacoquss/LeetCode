61d1791b893410279a53d5b71220badad230d34d
yacoquss <yacoquss@mail.ru>
2025-10-13 20:00:03 +0300

    ex

---
+++ Leetcode/2574. Left and Right Sum Differences/program.cs
@@ -0,0 +1,25 @@
+﻿namespace TestApp._2574._Left_and_Right_Sum_Differences;
+
+public class program
+{
+    public class Solution {
+        public int[] LeftRightDifference(int[] nums) {
+            int[] result = new int[nums.Length], leftSumArr = new int[nums.Length], rightSumArr = new int[nums.Length];
+            int leftSum = 0, rightSum = 0;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                leftSumArr[j] = leftSum;
+                leftSum += nums[j];
+
+                rightSumArr[nums.Length - 1 - j] = rightSum;
+                rightSum += nums[nums.Length - 1 - j];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+                result[i] = Math.Abs(leftSumArr[i] - rightSumArr[i]);
+
+            return result;
+        }
+    }
+}
