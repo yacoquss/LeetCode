e4430fa41b2d21c036e9e96e509665a196a5862c
yacoquss <yacoquss@mail.ru>
2025-10-16 19:31:59 +0300

    ex

---
+++ Leetcode/2956. Find Common Elements Between Two Arrays/program.cs
@@ -0,0 +1,25 @@
+﻿namespace TestApp._2956._Find_Common_Elements_Between_Two_Arrays;
+
+public class program
+{
+    
+}
+
+public class Solution {
+    public int[] FindIntersectionValues(int[] nums1, int[] nums2) {
+        List<int> result = new List<int>();
+        int first_answer = 0;
+        int second_answer = 0;
+        foreach (var num in nums1)
+        {
+            if(nums2.Contains(num)) first_answer++;
+        }
+        foreach (var num in nums2)
+        {
+            if(nums1.Contains(num)) second_answer++;
+        }
+        result.Add(first_answer);
+        result.Add(second_answer);
+        return result.ToArray();
+    }
+}
