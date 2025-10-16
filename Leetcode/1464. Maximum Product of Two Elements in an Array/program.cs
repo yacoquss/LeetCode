6893b37d160423c4ad2c0a4245ae4944931e1aca
yacoquss <yacoquss@mail.ru>
2025-10-16 20:35:54 +0300

    ex

---
+++ Leetcode/1464. Maximum Product of Two Elements in an Array/program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp._1464._Maximum_Product_of_Two_Elements_in_an_Array;
+
+public class program
+{
+    public int MaxProduct(int[] nums)
+    {
+        int max = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                max = Math.Max(max, (nums[i] - 1) * (nums[j] - 1));
+            }
+        }
+
+        return max;
+    }
+}
