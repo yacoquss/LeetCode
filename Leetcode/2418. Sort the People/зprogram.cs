efe38e28ae42f5cda8975b52889c46e4d331047a
yacoquss <yacoquss@mail.ru>
2025-10-16 19:19:24 +0300

    ex

---
+++ Leetcode/2418. Sort the People/зprogram.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp._2418._Sort_the_People;
+
+public class зprogram
+{
+    public string[] SortPeople(string[] names, int[] heights) {
+        
+        return names
+            .Select((name, index) => (name, height: heights[index]))
+            .OrderByDescending(x => x.height)
+            .Select(x => x.name)
+            .ToArray();
+    }
+}
