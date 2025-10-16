a24b6ff4f70c5104f4aaf4625e0df08a5ead114e
yacoquss <yacoquss@mail.ru>
2025-10-16 19:26:33 +0300

    ex

---
+++ Leetcode/1534. Count Good Triplets/program.cs
@@ -0,0 +1,34 @@
+﻿namespace TestApp._1534._Count_Good_Triplets;
+
+public class program
+{
+    public class Solution {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if ((0 <= i) & i < j & j < k & k < arr.Length)
+                        {
+                            if (Math.Abs(arr[i] - arr[j]) <= a)
+                            {
+                                if (Math.Abs(arr[j] - arr[k]) <= b)
+                                {
+                                    if (Math.Abs(arr[i] - arr[k]) <= c)
+                                    {
+                                        count++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
