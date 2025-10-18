10f5ed7cd40ba8aa5bb3d14b6724dadb78a28ca5
yacoquss <yacoquss@mail.ru>
2025-10-18 19:59:11 +0300

    ex

---
+++ Leetcode/202. Happy Number/program.cs
@@ -0,0 +1,25 @@
+﻿namespace TestApp._202._Happy_Number;
+
+public class program
+{
+    public bool IsHappy(int n) {
+        HashSet<int> seen = new HashSet<int>();
+    
+        while (n != 1 && !seen.Contains(n)) {
+            seen.Add(n);
+            n = GetNextNumber(n);
+        }
+    
+        return n == 1;
+    }
+
+    private int GetNextNumber(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
