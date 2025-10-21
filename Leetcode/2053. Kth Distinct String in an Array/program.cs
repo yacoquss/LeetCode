a3d2b25685719eaca53798ae954c027172122f56
yacoquss <yacoquss@mail.ru>
2025-10-21 21:42:38 +0300

    ex

---
+++ Leetcode/2053. Kth Distinct String in an Array/program.cs
@@ -0,0 +1,34 @@
+﻿namespace TestApp._2053._Kth_Distinct_String_in_an_Array;
+
+public class program
+{
+    public class Solution {
+        public string KthDistinct(string[] arr, int k) {
+        
+            Dictionary<string, int> count = new Dictionary<string, int>();
+        
+        
+            foreach (string s in arr) {
+                if (count.ContainsKey(s)) {
+                    count[s]++;
+                } else {
+                    count[s] = 1;
+                }
+            }
+        
+        
+            int distinctCount = 0;
+            foreach (string s in arr) {
+                if (count[s] == 1) {  
+                    distinctCount++;
+                    if (distinctCount == k) {
+                        return s;  
+                    }
+                }
+            }
+        
+        
+            return "";
+        }
+    }
+}
