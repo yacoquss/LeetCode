bc2f566a579ebfba68f53c81912e5b567684680f
yacoquss <yacoquss@mail.ru>
2025-10-21 21:27:41 +0300

    ex

---
+++ Leetcode/3174. Clear Digits/program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace TestApp._3174._Clear_Digits;
+
+public class program
+{
+    public class Solution {
+        public string ClearDigits(string s) {
+            StringBuilder result = new StringBuilder();
+        
+            foreach (char c in s) {
+                if (char.IsDigit(c)) {
+                
+                    if (result.Length > 0) {
+                        result.Length--; 
+                    }
+                } else {
+                
+                    result.Append(c);
+                }
+            }
+        
+            return result.ToString();
+        }
+    }
+}
