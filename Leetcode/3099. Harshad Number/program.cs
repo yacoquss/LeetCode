7bd98011b3726f47217beb7627dd2b6909d13292
yacoquss <yacoquss@mail.ru>
2025-10-21 21:36:56 +0300

    ex

---
+++ Leetcode/3099. Harshad Number/program.cs
@@ -0,0 +1,25 @@
+﻿namespace TestApp._3099._Harshad_Number;
+
+public class program
+{
+    public class Solution {
+        public int SumOfTheDigitsOfHarshadNumber(int x)
+        {
+            var sum = SumOfDigits(x);
+            if (x % sum == 0 && x % sum == 0) return sum;
+
+            return -1;
+        }
+
+        int SumOfDigits(int x)
+        {
+            int result = 0;
+            while (x > 0)
+            {
+                result +=  x % 10;
+                x /= 10;
+            }
+            return result;
+        }
+    }
+}
