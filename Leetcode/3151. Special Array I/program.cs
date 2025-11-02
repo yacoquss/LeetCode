52192083e195d87c22a507475101242cee2d030e
yacoquss <yacoquss@mail.ru>
2025-11-02 14:07:07 +0300

    ex

---
+++ Leetcode/3151. Special Array I/program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp._3151._Special_Array_I;
+
+public class program
+{
+    public class Solution {
+        public bool IsArraySpecial(int[] nums) {
+            if (nums.Length == 1) return true;
+        
+            for (int i = 0; i < nums.Length - 1; i++) {
+                // Check if adjacent elements have the same parity
+                if (nums[i] % 2 == nums[i + 1] % 2) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
