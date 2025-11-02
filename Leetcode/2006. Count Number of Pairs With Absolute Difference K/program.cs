c40a281f4c06603efcbc1ec18b10efe036c23a9b
yacoquss <yacoquss@mail.ru>
2025-11-02 13:53:27 +0300

    ex

---
+++ Leetcode/2006. Count Number of Pairs With Absolute Difference K/program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestApp._2006._Count_Number_of_Pairs_With_Absolute_Difference_K;
+
+public class Solution {
+    public int CountKDifference(int[] nums, int k)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i; j < nums.Length; j++)
+            {
+                if(i<j & Math.Abs(nums[i] - nums[j]) == k)
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
