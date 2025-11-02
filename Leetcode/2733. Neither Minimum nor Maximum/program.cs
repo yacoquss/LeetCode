0abe6ccf18bb3c84825b1dd1b88b2a290fd978d4
yacoquss <yacoquss@mail.ru>
2025-11-02 14:15:12 +0300

    ex

---
+++ Leetcode/2733. Neither Minimum nor Maximum/program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp._2733._Neither_Minimum_nor_Maximum;
+
+public class program
+{
+    public class Solution {
+        public int FindNonMinOrMax(int[] nums) {
+            if (nums.Length < 3) return -1;
+        
+            Array.Sort(nums);
+            return nums[1]; 
+        }
+    }
+}
