e6fc03e14916c36b77ba7af437848d820384bb6d
yacoquss <yacoquss@mail.ru>
2025-11-02 13:50:20 +0300

    ex

---
+++ Leetcode/2652. Sum Multiples/program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestApp._2652._Sum_Multiples;
+
+public class program
+{
+    public class Solution {
+        public int SumOfMultiples(int n) {
+            return Enumerable.Range(1, n)
+                .Where(i => i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                .Sum();
+        }
+    }
+}
