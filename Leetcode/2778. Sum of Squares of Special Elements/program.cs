ae877eed8bbd7b07f36d53f5706bfe7f9b2e79d5
yacoquss <yacoquss@mail.ru>
2025-11-02 14:01:50 +0300

    ex

---
+++ Leetcode/2778. Sum of Squares of Special Elements/program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestApp._2778._Sum_of_Squares_of_Special_Elements;
+
+public class Solution {
+    public int SumOfSquares(int[] nums) {
+        int sum = 0;
+        int n = nums.Length;
+        
+        for (int i = 0; i < n; i++) {
+            
+            if (n % (i + 1) == 0) {
+                sum += nums[i] * nums[i];
+            }
+        }
+        
+        return sum;
+    }
+}
